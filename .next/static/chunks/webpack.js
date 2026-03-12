b250dc8f4aae422f818b9fed59665ec1123b9a60
medicointernista <localrankmedellin@gmail.com>
2026-03-12 09:48:17 -0500

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a57457283711e913")
+/******/ 		__webpack_require__.h = () => ("2f38eadbb8b8588f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/cardiologia/holter-24-horas/page.tsx
+++ src/app/cardiologia/holter-24-horas/page.tsx
@@ -70,7 +70,7 @@ const Holter24HorasPage = () => {
         {/* Hero Image */}
         <figure className="mb-12 overflow-hidden rounded-[2rem] border-2 border-[#E2E8F0] bg-white/70 shadow-sm">
           <img
-            src="https://images.pexels.com/photos/5327585/pexels-photo-5327585.jpeg?auto=compress&cs=tinysrgb&w=1200"
+            src="/holter24horas.jpg"
             alt="Paciente con dispositivo Holter de 24 horas"
             className="w-full h-[300px] md:h-[450px] object-cover"
           />
